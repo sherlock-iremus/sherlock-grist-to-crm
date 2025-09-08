523487fb360000f5038fb68abc8c3dff16b6139f
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2025-09-08 14:41:54 +0200

    🌲

--- scripts/make-project-data.sh
+++ scripts/make-project-data.sh
@@ -3,6 +3,4 @@ source $SCRIPT_DIR/../ENV
 ROOT="$(dirname "$SCRIPT_DIR")"
 source ./venv/bin/activate
 
-mkdir -p $ROOT/out/ttl/grist/projects/
-
 ROOT=$ROOT sh $ROOT/scripts/user-projects-case.sh $1
--- scripts/make-sherlock-data.sh
+++ scripts/make-sherlock-data.sh
@@ -3,8 +3,8 @@ source $SCRIPT_DIR/../ENV
 ROOT="$(dirname "$SCRIPT_DIR")"
 source ./venv/bin/activate
 
-rm -rf $ROOT/out/ttl/grist/sherlock
-mkdir -p $ROOT/out/ttl/grist/sherlock
+rm -rf $OUTPUT_TTL_ROOT/grist/sherlock
+mkdir -p $OUTPUT_TTL_ROOT/grist/sherlock
 
 common_args="
 --grist_api_key $GRIST_API_KEY
@@ -15,55 +15,55 @@ common_args="
 # E21 Personnes
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_PERSONS \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-PERSONS.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/SHERLOCK-PERSONS.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E21_Person \
 
 # Projets
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_PROJECTS \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-PROJECTS.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/grist/sherlock/SHERLOCK-PROJECTS.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E7_Activity \
     --p2_has_type http://data-iremus.huma-num.fr/id/58c38fd3-ca35-476a-aa39-9cc815ee2dab \
 
 # Collections
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_COLLECTIONS \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-COLLECTIONS.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/grist/sherlock/SHERLOCK-COLLECTIONS.ttl \
     --rdf_type http://data-iremus.huma-num.fr/ns/sherlock#Collection \
 
 # Fichiers des projets
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_PROJECTS_FILES \
+    --output_ttl $OUTPUT_TTL_ROOT/grist/sherlock/SHERLOCK-PROJECTS-FILES.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E31_Document \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-PROJECTS-FILES.ttl \
 
 # E35 E55
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_E35_E55 \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-E35-E55.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/sherlock/SHERLOCK-E35-E55.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E55_Type \
     
 # E41 E55
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_E41_E55 \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-E41-E55.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/sherlock/SHERLOCK-E41-E55.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E55_Type \
     
 # E42 E55
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_E42_E55 \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-E42-E55.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/sherlock/SHERLOCK-E42-E55.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E55_Type \
     
 # P3 E55
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
     --grist_table_id SHERLOCK_P3_E55 \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-P3-E55.ttl \
+    --output_ttl $OUTPUT_TTL_ROOT/grist/sherlock/SHERLOCK-P3-E55.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E55_Type \
     
 # P177 E55
 python3 -m sherlock_grist_to_crm.grist-to-crm $common_args \
-    --grist_table_id SHERLOCK_P177_E55 \
     --e32_uuid 3abbb495-7105-4066-89fe-9d4b0474e492 \
-    --output_ttl $ROOT/out/ttl/grist/sherlock/SHERLOCK-E13-E55.ttl \
+    --grist_table_id SHERLOCK_P177_E55 \
+    --output_ttl $OUTPUT_TTL_ROOT/grist/sherlock/SHERLOCK-E13-E55.ttl \
     --rdf_type http://www.cidoc-crm.org/cidoc-crm/E55_Type \
